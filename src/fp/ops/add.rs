03dd52b532314dfc1019624f9d6d57e107c041a1
Asger Hautop Drewsen <asger@tyilo.com>
2026-03-19 19:43:02 +0100

    Implement AddAssign<&Self> for Fp

--- src/fp/ops/add.rs
+++ src/fp/ops/add.rs
@@ -26,6 +26,17 @@ where
         *self = *self + rhs
     }
 }
+impl<U: UInt, const SIGN_BIT: bool, const EXP_SIZE: usize, const INT_SIZE: usize, const FRAC_SIZE: usize, const EXP_BASE: usize> AddAssign<&Self> for Fp<U, SIGN_BIT, EXP_SIZE, INT_SIZE, FRAC_SIZE, EXP_BASE>
+where
+    [(); util::bitsize_of::<U>() - SIGN_BIT as usize - EXP_SIZE - INT_SIZE - FRAC_SIZE]:,
+    [(); EXP_BASE - 2]:
+{
+    #[inline]
+    fn add_assign(&mut self, rhs: &Self)
+    {
+        *self = *self + *rhs
+    }
+}
 
 #[cfg(test)]
 mod test
